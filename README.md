dbb447ec7f134cb11f613944a7aa8c3d825eea20
Julien V <vier.julien@googlemail.com>
2022-08-22 17:25:51 +0100

    Added library features for V2

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # mg-url-encoder
 MG URL Encoder - A URL Encoder for CDN
+
+JS to add to library with HTML:
+<!-- <div class="library__row">
+    <div class="row__icon row_${counter}">
+        <span class="material-symbols-outlined">
+            link
+        </span>
+    </div>
+    <div class="row__url">
+        ${finalOutput}
+    </div>
+    <div class="row__copy">
+        <span class="material-symbols-outlined">
+            ${assetIcon}
+        </span>
+    </div>
+</div> -->
--- app.js
+++ app.js
@@ -12,7 +12,6 @@ const fontCheckbox = document.querySelector('#font');
 const iconCheckbox = document.querySelector('#icon');
 
 // DISPLAY WIDTH PROPERTY IF IMAGE
-
 imageCheckbox.addEventListener('change', function() {
     if (this.checked) {
         imageWidth.style.display = "block";
@@ -20,6 +19,13 @@ imageCheckbox.addEventListener('change', function() {
         imageWidth.style.display = "none";
     }
 });
+
+// LIBRARY
+const storedLibrary = localStorage.getItem("libraryitems");
+if (linkLibrary) {
+    linkLibrary.innerHTML = storedLibrary;
+};
+
 
 function encodeFunction() {
 
@@ -34,31 +40,73 @@ function encodeFunction() {
     // GENERATE OUTPUT
     let finalOutput;
 
+    // PICK ASSET ICON FOR LIBRARY
+    let assetIcon;
+
     // ENCODE URL
     if (imageCheckbox.checked === true && fontCheckbox.checked === false && iconCheckbox.checked === false) {
         const encodedUrl = encodeURIComponent(urlInput);
         finalOutput = urlPrefix + encodedUrl + urlSuffix;
         outputResult.value = finalOutput;
+        assetIcon = "image";
     } else if (fontCheckbox.checked === true && imageCheckbox.checked === false && iconCheckbox.checked === false) {
         finalOutput = urlInput.replace("musicgluewwwassets.s3.amazonaws.com", "musicglue-wwwassets.global.ssl.fastly.net");
         outputResult.value = finalOutput;
+        assetIcon = "text_fields";
     } else if (iconCheckbox.checked === true && fontCheckbox.checked === false && imageCheckbox.checked === false) {
         finalOutput = urlInput.replace("musicgluewwwassets.s3.amazonaws.com", "musicglue-wwwassets.global.ssl.fastly.net");
         outputResult.value = finalOutput;
+        assetIcon = "language";
     }  else {
         alert("ERROR! You need to tick at least one of the check boxes")
     }
 
+    
+
     // SAVE TO LIBRARY
-    linkLibrary.innerHTML += `<p>${finalOutput}</p>`;
+    linkLibrary.insertAdjacentHTML("afterbegin", `
+    <div class="library__row">
+        <div class="row__icon">
+            <a id="library-url-copy">
+            <span class="material-symbols-outlined" >
+                link
+            </span>
+            </a>
+        </div>
+        <div class="row__url" id="library-url">
+            ${finalOutput}
+        </div>
+        <div class="row__copy">
+            
+            <span class="material-symbols-outlined">
+                <a class="thumbnail" href="#thumb">
+                    ${assetIcon}
+                    <span>
+                        <img width="250px" src="${finalOutput}">
+                    </span>
+                </a>
+            </span>
+
+        </div>
+    </div>
+        `);
+
 }
+
+
+
+const saveToLocalStorage = () => {
+    localStorage.setItem("libraryitems", linkLibrary.innerHTML);
+};
 
 
 const button = document.getElementById('encode');
 button.addEventListener("click", encodeFunction);
+button.addEventListener("click", saveToLocalStorage);
 
 // Copy output to clipboard
 outputCopy.addEventListener("click", function() {
     outputResult.select();
     document.execCommand("Copy");
 });
+
